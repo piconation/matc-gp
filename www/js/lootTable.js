e5a1501e5aac2a8312845be7c83ed423c671ab3c
Isaac <theicean@gmail.com>
2016-09-13 09:45:58 -0600

    objects

---
+++ www/js/lootTable.js
@@ -0,0 +1,39 @@
+/**
+ * Created by isaacwatts on 9/13/16.
+ */
+
+var loot = [
+  {
+    "name": "metal",
+    "lid": 0
+  },
+  {
+    "name": "food",
+    "lid": 1
+  },
+  {
+    "name": "medkit",
+    "lid": 2
+  },
+  {
+    "name": "fuel",
+    "lid": 3
+  },
+  {
+    "name": "wires",
+    "lid": 4
+  },
+  {
+    "name": "electronics",
+    "lid": 5
+  },
+  {
+    "name": "wood",
+    "lid": 6
+  },
+  {
+    "name": "batteries",
+    "lid": 7
+  },
+
+];
