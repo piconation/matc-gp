7eb0094a75c32a9721d35630a5dcf03a31e10706
mattpowell <piconation@yahoo.com>
2016-09-16 10:24:35 -0600

    MATT

---
+++ www/js/homebase.component.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mattpowell on 9/16/16.
+ */
+
+(function () {
+  angular.module('starter')
+    .component('homebase', {
+      templateUrl: "templates/homebase.html",
+      controller: homebaseController
+    })
+  function homebaseController(user, $state){
+    var self = this;
+    self.
+  }
+})
