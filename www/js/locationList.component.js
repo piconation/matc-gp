fb32ac59b4d0fe20c71c34d0c3451ca735eeb032
Isaac <theicean@gmail.com>
2016-09-27 08:44:46 -0600

    old and desc to locale added

--- www/js/locationList.component.js
+++ www/js/locationList.component.js
@@ -14,27 +14,32 @@
       {
         'id': 0,
         'sortkey': "establish odds",
-        'name': 'Classroom'
-      },
+        'name': 'Homebase',
+        'desc': "The beginning of your empire."
+              },
       {
         'id': 1,
         'sortkey': "establish odds",
-        'name': 'Mall'
+        'name': 'Mall',
+        'desc': "No one seems to be around."
       },
       {
         'id': 2,
         'sortkey': "establish odds",
-        'name': 'Junkyard'
+        'name': 'Junkyard',
+        'desc': "Thank goodness there aren't any dogs around."
       },
       {
         'id': 3,
         'sortkey': "establish odds",
-        'name': 'Woods'
+        'name': 'Woods',
+        'desc': "Not even the sound of birds. It is unnervingly silent."
       },
       {
         'id': 4,
         'sortkey': "establish odds",
-        'name': 'Homebase'
+        'name': 'School',
+        'desc': "Hey look, an old school house."
       }
     ];
   }
